52099c02e0ef0dc0354f601f0239addc853aad33
bsilv22 <bsilv22@yahoo.com>
2022-10-05 12:48:34 -0400

    Initial commit

---
+++ Assets/Scripts/RotateMe.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMe : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed = 10.0f;
+    
+    void Update()
+    {
+        transform.RotateAround(target.transform.position,
+            target.transform.up,
+            speed * Time.deltaTime);
+    }
+}
---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,61 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void CameraSwitch::Start()
+extern void CameraSwitch_Start_m79D5F9F731AFFF6D5339A7C48A4E185759839F55 (void);
+// 0x00000002 System.Void CameraSwitch::Update()
+extern void CameraSwitch_Update_m987836CE4C719B0EF2784ABD4AB33EFCE4BEDA23 (void);
+// 0x00000003 System.Void CameraSwitch::.ctor()
+extern void CameraSwitch__ctor_m353D283057D459AABE2512B589AA33005E29595A (void);
+// 0x00000004 System.Void RotateMe::Update()
+extern void RotateMe_Update_mDFBA3D7DED2FCD15C6BC8E294094B5A88FD98D5B (void);
+// 0x00000005 System.Void RotateMe::.ctor()
+extern void RotateMe__ctor_m9C895FE153BFD4B4C51A5C870E9825141D1E55B1 (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	CameraSwitch_Start_m79D5F9F731AFFF6D5339A7C48A4E185759839F55,
+	CameraSwitch_Update_m987836CE4C719B0EF2784ABD4AB33EFCE4BEDA23,
+	CameraSwitch__ctor_m353D283057D459AABE2512B589AA33005E29595A,
+	RotateMe_Update_mDFBA3D7DED2FCD15C6BC8E294094B5A88FD98D5B,
+	RotateMe__ctor_m9C895FE153BFD4B4C51A5C870E9825141D1E55B1,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	1020,
+	1020,
+	1020,
+	1020,
+	1020,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
